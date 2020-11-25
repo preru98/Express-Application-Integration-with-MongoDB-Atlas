01f7d975c5ebfd91a498d63ce004296ad1faffe4
Prerna <prerna1998.ps@gmail.com>
2020-11-25 19:41:54 +0530

    STEP 4: Write routes/API for previously developed Model (Test)

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const testRouter  = require('./routes/test');
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/perform', testRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
